60f7191ff5e9c5935b4340143a13609a271e252a
cokikipla <cokicollinji@gmail.com>
2021-07-08 10:01:48 +0300

    Initial version of Employee generated by generator-jhipster@7.1.0

---
+++ src/test/java/com/coki/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.coki;
+
+import com.coki.EmployeeApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = EmployeeApp.class)
+public @interface IntegrationTest {
+}
